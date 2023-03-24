1b9dabe8f8894df3688cc4557958fe4ba15bb31d
alinabodnar1 <malina.bodnar@gmail.com>
2023-03-24 16:22:22 +0200

    add hw-10

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://restcountries.com/v3.1/name/";
+const FILTRED_FIELDS = "name,capital,population,flags,languages";
+
+export function getCountries(name) {
+    return fetch(BASE_URL + `${name}?fields=${FILTRED_FIELDS}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        });   
+}
